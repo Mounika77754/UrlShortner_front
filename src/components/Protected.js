f4673c9d65c692987a09725a7157e7d24995ffe1
Mounika77754 <mouni77754@gmail.com>
2025-10-07 14:42:38 +0530

    done

--- src/components/Protected.js
+++ src/components/Protected.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 export default function Protected(props){
     if(!localStorage.getItem("token")){
-        return <Navigate to="/" replace/>
+        return <Navigate to="/login" replace/>
     }
     return props.children
 }
